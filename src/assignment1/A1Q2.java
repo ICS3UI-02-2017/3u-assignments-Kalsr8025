87dd0593b414721f9e4b1b9f50180953b77753a1
Ripanpreet K. <Kalsr8025@wrdsb.ca>
2018-02-12 17:08:47 -0500

    Complete A1Q1 and A!Q2

--- src/assignment1/A1Q2.java
+++ src/assignment1/A1Q2.java
@@ -4,6 +4,13 @@
  */
 package assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
 /**
  *
  * @author Kalsr8025
@@ -14,6 +21,68 @@ public class A1Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create a city for robot//
+        City kalsi = new City ();
+        
+        //place the robot in the city//
+        RobotSE robo = new RobotSE (kalsi,1,2,Direction.SOUTH);
+        
+        //Change the colour of robo//
+        robo.setColor(Color.MAGENTA);
+        
+        //Make walls in kalsi//
+        new Wall (kalsi,1,1,Direction.NORTH);
+        new Wall (kalsi,1,2,Direction.NORTH);
+        new Wall (kalsi,1,2,Direction.EAST);
+        new Wall (kalsi,1,2,Direction.SOUTH);
+        new Wall (kalsi,1,1,Direction.WEST);
+        new Wall (kalsi,2,1,Direction.WEST);
+        new Wall (kalsi,2,1,Direction.SOUTH);
+        
+        //place the newspaper on door//
+        new Thing (kalsi,2,2);
+        
+        //turn right //
+        robo.turnRight();
+        
+        //move forward//
+        robo.move();
+        
+        //turn left//
+        robo.turnLeft();
+        
+        //move forward//
+        robo.move();
+        
+        //turn left//
+        robo.turnLeft();
+        
+        //move forward//
+        robo.move();
+        
+        //pick the thing up//
+        robo.pickThing();
+        
+        //turn around//
+        robo.turnAround();
+        
+        //move forward//
+        robo.move();
+        
+        //turn right//
+        robo.turnRight();
+        
+        //move forward//
+        robo.move();
+        
+        //turn right//
+        robo.turnRight();
+        
+        //move foward//
+        robo.move();
+        
+        //robo stands in its orginal position//
+        robo.turnRight();
+                
     }
 }
