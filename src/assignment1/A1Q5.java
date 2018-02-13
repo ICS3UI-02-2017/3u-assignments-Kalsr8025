550f83b42b711b0cf8b6aa88be56c6dec17dfdf0
Kalsr8025 <Kalsr8025@HRH-IC0027343.wrdsb.ca>
2018-02-13 10:48:34 -0500

    Complete

--- src/assignment1/A1Q5.java
+++ src/assignment1/A1Q5.java
@@ -4,6 +4,12 @@
  */
 package assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
 /**
  *
  * @author Kalsr8025
@@ -14,6 +20,49 @@ public class A1Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create a city for robot//
+      City kalsi = new City ();
+      
+      //place robot k and robot m in kalsi//
+      RobotSE robotm = new RobotSE (kalsi,0,1,Direction.WEST);
+      RobotSE robotk = new RobotSE (kalsi,3,3,Direction.EAST);
+      
+      //Placing walls in kalsi//
+      new Wall (kalsi,2,3,Direction.WEST);
+      new Wall (kalsi,2,3,Direction.NORTH); 
+      new Wall (kalsi,2,3,Direction.EAST);
+      new Wall (kalsi,3,3,Direction.EAST);
+      new Wall (kalsi,3,3,Direction.SOUTH);
+      
+      //Labeling robot k and robot m =//
+      robotk.setLabel("K");
+      robotm.setLabel("M");
+      
+      //Placing newspaper in kalsi//
+      new Thing (kalsi,0,0);
+      new Thing (kalsi,1,0);
+      new Thing (kalsi,1,1);
+      new Thing (kalsi,1,2);
+      new Thing (kalsi,2,2);
+      
+      //Robot k moves to pick things//
+      robotk.turnAround();
+      robotk.move();
+      robotk.turnRight();
+      robotk.move();
+      robotk.pickThing();
+      robotk.move();
+      robotk.pickThing();
+      robotk.turnLeft();
+      
+      //Robot m moves to pick things//
+      robotm.move();
+      robotm.pickThing();
+      robotm.turnLeft();
+      robotm.move();
+      robotm.pickThing();
+      robotm.turnLeft();
+      robotm.move();
+      robotm.pickThing();
     }
 }
