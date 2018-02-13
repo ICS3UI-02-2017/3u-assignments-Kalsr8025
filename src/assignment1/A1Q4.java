434e0eebd82440ee41d0bcfa3ce76e0655735fd9
Kalsr8025 <36271874+Kalsr8025@users.noreply.github.com>
2018-02-12 19:04:39 -0500

    Complete

--- src/assignment1/A1Q4.java
+++ src/assignment1/A1Q4.java
@@ -4,6 +4,11 @@
  */
 package assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author Kalsr8025
@@ -14,6 +19,45 @@ public class A1Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create the city//
+        City kalsi = new City ();
+        
+        //place the robot in kalsi//
+        RobotSE robo1 = new RobotSE (kalsi,0,0,Direction.SOUTH);
+        
+        //place the robot 2 in kalsi//
+        RobotSE robo2 = new RobotSE (kalsi,0,1,Direction.SOUTH);
+        
+        //place the wall//
+        new Wall (kalsi,0,1,Direction.WEST);
+        new Wall (kalsi,1,1,Direction.WEST);
+        new Wall (kalsi,1,1,Direction.SOUTH);
+        
+        //Robo2 moves forward//
+        robo2.move();
+        
+        //Robo2 turns left//
+        robo2.turnLeft();
+        
+        //Robo2 moves forward//
+        robo2.move();
+        
+        //Robo2 turns right//
+        robo2.turnRight();
+        
+        //Robo1 moves forward 2)
+        robo1.move(2);
+        
+        //Robo2 moves forward//
+        robo2.move();
+        
+        //Robo1 turns left//
+        robo1.turnLeft();
+        
+        //Robo2 turns right//
+        robo2.turnRight();
+        
+        //Robo1 and Robo2 moves forwards//
+        robo1.move(); robo2.move();
     }
 }
