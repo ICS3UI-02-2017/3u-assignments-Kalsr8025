0fb013526e300446816abded21190681f2c3d41b
Kalsr8025 <36271874+Kalsr8025@users.noreply.github.com>
2018-02-23 11:48:06 -0500

    Start of assignment 3

---
+++ src/assignment3/A3Q2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+
+/**
+ *
+ * @author kalsr8025
+ */
+public class A3Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create the city 
+        City kalsi = new City ();
+        
+        //create the robot
+        RobotSE robo = new RobotSE (kalsi,0,2,Direction.WEST);
+    }
+}
