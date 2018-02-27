6a0b1c2110bbdf025dbd62988d6d134cf42a5022
Kalsr8025 <36271874+Kalsr8025@users.noreply.github.com>
2018-02-27 11:17:44 -0500

    Update A3Q4.java

--- src/assignment3/A3Q4.java
+++ src/assignment3/A3Q4.java
@@ -7,6 +7,7 @@ package assignment3;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Wall;
 
 /**
  *
@@ -19,9 +20,67 @@ public class A3Q4 {
      */
     public static void main(String[] args) {
         //create the city 
-        City kalsi = new City ();
-        
+        City kalsi = new City();
+
         //create the robot
-        RobotSE robo = new RobotSE (kalsi,0,2,Direction.WEST);
+        RobotSE robo = new RobotSE(kalsi, 3, 3, Direction.SOUTH);
+
+        //create walls 
+        new Wall(kalsi, 1, 1, Direction.WEST);
+        new Wall(kalsi, 2, 1, Direction.WEST);
+        new Wall(kalsi, 4, 1, Direction.WEST);
+        new Wall(kalsi, 5, 1, Direction.WEST);
+        new Wall(kalsi, 1, 4, Direction.WEST);
+        new Wall(kalsi, 2, 4, Direction.WEST);
+        new Wall(kalsi, 4, 4, Direction.WEST);
+        new Wall(kalsi, 5, 4, Direction.WEST);
+
+        new Wall(kalsi, 1, 1, Direction.NORTH);
+        new Wall(kalsi, 1, 2, Direction.NORTH);
+        new Wall(kalsi, 1, 4, Direction.NORTH);
+        new Wall(kalsi, 1, 5, Direction.NORTH);
+        new Wall(kalsi, 4, 1, Direction.NORTH);
+        new Wall(kalsi, 4, 2, Direction.NORTH);
+        new Wall(kalsi, 4, 4, Direction.NORTH);
+        new Wall(kalsi, 4, 5, Direction.NORTH);
+
+        new Wall(kalsi, 1, 2, Direction.EAST);
+        new Wall(kalsi, 2, 2, Direction.EAST);
+        new Wall(kalsi, 4, 2, Direction.EAST);
+        new Wall(kalsi, 5, 2, Direction.EAST);
+        new Wall(kalsi, 1, 5, Direction.EAST);
+        new Wall(kalsi, 2, 5, Direction.EAST);
+        new Wall(kalsi, 4, 5, Direction.EAST);
+        new Wall(kalsi, 5, 5, Direction.EAST);
+
+        new Wall(kalsi, 2, 1, Direction.SOUTH);
+        new Wall(kalsi, 2, 2, Direction.SOUTH);
+        new Wall(kalsi, 2, 4, Direction.SOUTH);
+        new Wall(kalsi, 2, 5, Direction.SOUTH);
+        new Wall(kalsi, 5, 1, Direction.SOUTH);
+        new Wall(kalsi, 5, 2, Direction.SOUTH);
+        new Wall(kalsi, 5, 4, Direction.SOUTH);
+        new Wall(kalsi, 5, 5, Direction.SOUTH);
+
+
+        //the robo moves  
+        robo.move(3);
+        robo.turnLeft();
+        robo.move(3);
+        robo.turnLeft();
+        robo.move(3);
+        robo.turnLeft();
+
+        for (int robotomove = 0; robotomove < 3; robotomove++) {
+            robo.move(6);
+            robo.turnLeft();
+            robo.move(3);
+            robo.turnLeft();
+            robo.move(3);
+            robo.turnLeft();
+        }
+        //move to the start point 
+        robo.move(3);
+
     }
 }
