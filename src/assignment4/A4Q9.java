013cd1d7a8a473f24a1836edb5e2b876d9e97441
Ripanpreet K. <Kalsr8025@wrdsb.ca>
2018-03-16 19:41:07 -0400

    Working on A4Q10

--- src/assignment4/A4Q9.java
+++ src/assignment4/A4Q9.java
@@ -26,21 +26,203 @@ public class A4Q9 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        add = new javax.swing.JButton();
+        sub = new javax.swing.JButton();
+        div = new javax.swing.JButton();
+        mul = new javax.swing.JButton();
+        first = new javax.swing.JTextField();
+        second = new javax.swing.JTextField();
+        answer = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        add.setText("ADD");
+        add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addActionPerformed(evt);
+            }
+        });
+
+        sub.setText("SUB");
+        sub.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                subActionPerformed(evt);
+            }
+        });
+
+        div.setText("DIV");
+        div.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                divActionPerformed(evt);
+            }
+        });
+
+        mul.setText("MUL");
+        mul.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mulActionPerformed(evt);
+            }
+        });
+
+        first.setText("First number ");
+        first.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                firstActionPerformed(evt);
+            }
+        });
+
+        second.setText("Second number ");
+        second.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                secondActionPerformed(evt);
+            }
+        });
+
+        answer.setEditable(false);
+
+        jLabel1.setFont(new java.awt.Font("Snap ITC", 0, 14)); // NOI18N
+        jLabel1.setText("WELCOME TO CALCULATOR ");
+
+        jLabel2.setText("#");
+
+        jLabel3.setText("#");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(29, 29, 29)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(add)
+                                .addComponent(jLabel2)
+                                .addComponent(jLabel3))
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addGap(22, 22, 22)
+                                    .addComponent(sub)
+                                    .addGap(36, 36, 36)
+                                    .addComponent(div)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(mul))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(second, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
+                                    .addComponent(first, javax.swing.GroupLayout.Alignment.LEADING))))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(50, 50, 50)
+                            .addComponent(answer, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(first, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(second, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(sub)
+                    .addComponent(add)
+                    .addComponent(div)
+                    .addComponent(mul))
+                .addGap(18, 18, 18)
+                .addComponent(answer, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void firstActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_firstActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_firstActionPerformed
+
+    private void secondActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_secondActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_secondActionPerformed
+
+    private void addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addActionPerformed
+        //ask for the numbers 
+        String num1String = first.getText();
+        String num2String = second.getText();
+
+        //math and output it in the answer feild 
+        int num1 = Integer.parseInt(num1String);
+        int num2 = Integer.parseInt(num2String);
+
+        int ans = num1 + num2;
+
+        answer.setText("You're answer is " + ans);
+
+    }//GEN-LAST:event_addActionPerformed
+
+    private void subActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subActionPerformed
+        //ask for the numbers 
+        String num1String = first.getText();
+        String num2String = second.getText();
+
+        //math and output it in the answer feild 
+        int num1 = Integer.parseInt(num1String);
+        int num2 = Integer.parseInt(num2String);
+
+        int ans = num1 - num2;
+
+        answer.setText("You're answer is " + ans);
+
+    }//GEN-LAST:event_subActionPerformed
+
+    private void divActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_divActionPerformed
+        //ask for the numbers 
+        String num1String = first.getText();
+        String num2String = second.getText();
+
+        //math and output it in the answer feild 
+        int num1 = Integer.parseInt(num1String);
+        int num2 = Integer.parseInt(num2String);
+
+        int ans = num1 / num2;
+
+        answer.setText("You're answer is " + ans);
+
+    }//GEN-LAST:event_divActionPerformed
+
+    private void mulActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mulActionPerformed
+        //ask for the numbers 
+        String num1String = first.getText();
+        String num2String = second.getText();
+
+        //math and output it in the answer feild 
+        int num1 = Integer.parseInt(num1String);
+        int num2 = Integer.parseInt(num2String);
+
+        int ans = num1 * num2;
+
+        answer.setText("You're answer is " + ans);
+
+    }//GEN-LAST:event_mulActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,5 +259,17 @@ public class A4Q9 extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton add;
+    private javax.swing.JTextField answer;
+    private javax.swing.JButton div;
+    private javax.swing.JTextField first;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JButton mul;
+    private javax.swing.JTextField second;
+    private javax.swing.JButton sub;
     // End of variables declaration//GEN-END:variables
 }
