201af5b175a01eced0b1c51900a4df159d69344d
Ripanpreet K. <Kalsr8025@wrdsb.ca>
2018-03-25 17:45:39 -0400

    Stuck on UB for all of the vowels....

--- src/assignment5/UbbiDubbiTranslator.java
+++ src/assignment5/UbbiDubbiTranslator.java
@@ -21,12 +21,9 @@ public class UbbiDubbiTranslator {
 
         //1.The infix “ub” is added before each singular vowel in a given word
         //2.In the case of multiple vowels in a row, “ub” is added before the string of vowels.
-
         //Start of program 
         System.out.println("Hubellubo Thuberuber! Wubelcubomube tubo UbbbububiDubububbubi Trubansubaltubor");
-        System.out.println("In Simple form...Hello There! Welcome to UbbiDubbi Transaltor");
-        System.out.println("");
-        System.out.println("Lets make some cool words using UbbiDubbi rules..");
+        System.out.println("Type the word to translate into UbbiDubbi ..");
 
         // Transaltor code 
         while (true) {
@@ -34,20 +31,38 @@ public class UbbiDubbiTranslator {
             System.out.println("Enter a word to translate");
             //string fo the user's word 
             String original = input.nextLine();
+            char[] chars = original.toCharArray();
+
+            String translated = new String();
+
             //sanitize the word 
             original = original.toLowerCase();
 
-            //know the length of the 
+            //know the length of the word
             int length = original.length();
 
             //make a program so it check each letter to be a vowel or not; add a ub once 
             //and not place with twice vowel 
-            
-            //if (original.
-                    //|| original.startsWith("e")
-                    //|| original.startsWith("i")
-                   // || original.startsWith("o")
-                   // || original.startsWith("u")) {
+            for (int place = 1; place < length; place++) {
+                char vowel = original.charAt(place);
+
+                if (vowel == 'a'
+                        || vowel == 'e'
+                        || vowel == 'i'
+                        || vowel == 'o'
+                        || vowel == 'u') {
+                    //check if the last letter was not a vowel 
+                    if (original.charAt(place - 1) != 'a'
+                            || original.charAt(place - 1) != 'e'
+                            || original.charAt(place - 1) != 'i'
+                            || original.charAt(place - 1) != 'o'
+                            || original.charAt(place - 1) != 'u') {
+
+                    }
+                    translated += "ub";
+                }
+                translated += vowel;
+
             }
         }
     }
